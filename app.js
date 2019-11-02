94fd54cbae8b4bd9ce0468af333d00195ccaadd0
josephwhite <damasta21th@aol.com>
2019-11-02 14:15:59 -0400

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+function changeVideo() {
+  var rand = Math.floor(Math.random()*12)
+  var iframe = document.getElementById('playlist')
+  iframe.setAttribute('src', iframe.getAttribute('src').replace(/index=\d+/, 'index='+rand))
+}
+
+function changeWallpaper() {
+  var totalCount = 1;
+  var num = Math.ceil(Math.random()*totalCount);
+  document.body.background = 'images/'+num+'.jpg';
+  document.body.style.backgroundSize = "cover"; // Background repeat
+}
